ca9f95d3692a9566fb840228282268da238ba3f1
PirogArtyom <pirog.artyom@gmail.com>
2016-06-12 13:55:13 +0300

    0

---
+++ app/scripts.js
@@ -0,0 +1,13 @@
+var kefir = "нет", ryazenka = "нет", yogurt = "да";
+if (kefir == "да") {
+    document.write("Мы покупаем кефир");
+}
+else if (ryazenka == "да") {
+    document.write("Покупаем ряженку");
+}
+else if (yogurt == "да") {
+    document.write("Покупаем йогурт");
+}
+else {
+    document.write("Уходим из магазина");
+}
