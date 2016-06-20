dbb64971174057aa051a219015a9ee28ab77d42a
PirogArtyom <pirog.artyom@gmail.com>
2016-06-20 21:57:52 +0300

    0

---
+++ app/index.js
@@ -0,0 +1,27 @@
+// var myString = "Artyom";
+// console.log(myString.toUpperCase());
+// console.log(myString);
+
+// var a = 2;
+// var b = 5;
+// var c = a + b;
+// console.log(c);
+
+// var a, b, c, d;
+// a = b = c = d = 5;
+// console.log(a);
+
+// function calcTotalDistance(time, speedOne, speedTwo) {
+//     var OneDistance = time * speedOne;
+//     var TwoDistance = time * speedTwo;
+//     var TotalDistance = OneDistance + TwoDistance;
+//
+//     return TotalDistance;
+// }
+//     console.log(calcTotalDistance(4,5,8));
+
+
+
+
+
+
